cca720bf465927a4660c77939fc5e00a11d5c97f
David Cedeño <DavidCT280@gmail.com>
2026-06-01 00:52:44 +0200

    arreglado extras

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app.jsx'; 
-import '../styles/index.css';         // <--- ¡Asegúrate de que diga index.css aquí!
+import '../styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
